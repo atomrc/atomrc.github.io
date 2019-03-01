559eb8b69bfe7fa9690a2df5ca70d2e271e9d8dc
Thomas Belin <me@thomasbelin.fr>
2019-03-01 23:41:19 +0100

    feat: Remove read event tracker

--- js/application.js
+++ js/application.js
@@ -1,40 +1,10 @@
 (function(win, doc) {
-  function compile(directives, rootElement) {
-    Object.entries(directives).forEach(([selector, directive]) => {
-      rootElement
-        .querySelectorAll(selector)
-        .forEach(element => directive(element));
-    });
-  }
-
-  function probe(element, onVisible) {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(({ isIntersecting, target }) => {
-        if (isIntersecting) {
-          onVisible(target);
-          observer.unobserve(target);
-        }
-      });
-    });
-    observer.observe(element);
-  }
-
-  var directives = {
-    "article h2, article h3": element => {
-      var anchor = doc.createElement("a");
-      anchor.href = "#" + element.id;
-      anchor.classList.add("anchor");
-      element.appendChild(anchor);
-    },
-
-    "[data-reading-probe]": element => {
-      probe(element, element => {
-        ga("send", "event", "article", "read", element.dataset.readingProbe);
-      });
-    }
-  };
-
-  compile(directives, doc);
+  doc.querySelectorAll("article h2, article h3").forEach(element => {
+    var anchor = doc.createElement("a");
+    anchor.href = "#" + element.id;
+    anchor.classList.add("anchor");
+    element.appendChild(anchor);
+  });
 
   doc.addEventListener("DOMContentLoaded", () => {
     //hack to force the browser to interpret the
