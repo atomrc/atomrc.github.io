df65e6c393a728cfb3dc6343e868a0a590d419fd
Thomas Belin <thomasbelin4@gmail.com>
2021-05-26 12:32:49 +0200

    chore: add lighthouse ci testing (#12)

---
+++ lighthouse.config.js
@@ -0,0 +1,37 @@
+const parser = require("fast-xml-parser");
+const fs = require("fs");
+
+const data = fs.readFileSync("./_site/sitemap.xml", "utf-8");
+const urls = parser.parse(data).urlset.url;
+
+const urlsToTest = urls
+  .sort((a, b) => {
+    const dateA = new Date(a.lastmod);
+    const dateB = new Date(b.lastmod);
+    return dateA > dateB ? -1 : 1;
+  })
+  .slice(0, 2)
+  .map((url) => url.loc.replace("https://blog.atomrc.dev", ""));
+
+const config = {
+  ci: {
+    collect: {
+      staticDistDir: "./_site",
+      url: urlsToTest,
+      serverBaseUrl: "https://blog.atomrc.dev",
+    },
+    assert: {
+      preset: "lighthouse:no-pwa",
+      assertions: {
+        canonical: "off",
+        "link-name": "off",
+        "max-potential-fid": "off",
+        "uses-long-cache-ttl": "off",
+        "color-contrast ": "warn",
+      },
+    },
+  },
+};
+
+console.log("generated config", JSON.stringify(config, null, 4));
+module.exports = config;
