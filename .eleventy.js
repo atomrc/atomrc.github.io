9aad1af897d30fe19b1d798beae4a91177d84ea7
Thomas Belin <me@thomasbelin.fr>
2024-04-14 17:09:58 +0200

    fix: static image generation

--- .eleventy.js
+++ .eleventy.js
@@ -16,6 +16,7 @@ async function imageShortcode(src, alt) {
     widths: [300, 600, 900],
     formats: ["avif", "jpeg"],
     outputDir: "_site/img",
+    concurrency: 0,
   });
 
   let imageAttributes = {
